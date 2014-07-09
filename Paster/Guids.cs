0121b168cf7fdffec4c802bc294a0a115411552c
Graeme Bradbury <graeme@siliconshark.com>
2014-07-09 13:55:05 +0100

    Solution created

---
+++ Paster/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace SiliconSharkLtd.Paster
+{
+    static class GuidList
+    {
+        public const string guidPasterPkgString = "799565f1-457b-429a-8b06-39fb8de521fb";
+        public const string guidPasterCmdSetString = "859fa95e-bb4f-4ec7-8cf4-958679b31fc4";
+
+        public static readonly Guid guidPasterCmdSet = new Guid(guidPasterCmdSetString);
+    };
+}
