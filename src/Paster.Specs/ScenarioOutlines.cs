126b08afa5848c85f70392af7b8517e2e20c2b72
Graeme Bradbury <graeme@siliconshark.com>
2014-08-08 15:57:22 +0100

    Improved feedback from Scenario outline specification

--- src/Paster.Specs/ScenarioOutlines.cs
+++ src/Paster.Specs/ScenarioOutlines.cs
@@ -37,23 +37,52 @@ namespace Paster.Specs
 
             "When the gherkin is pasted".When(() => sut.PasteGherkin(clipboard));
 
-            "Then the environment receives a test method with examples".Then(() =>
-                                                                                 {
-                                                                                     var sb = new StringBuilder();
+            "Then the environment receives a test method with examples"
+                .Then(() =>
+                          {
+                              var sb = new StringBuilder();
 
-                                                                                     sb.AppendLine("[Scenario]");
-                                                                                     sb.AppendLine("[Example(12,5,7)]");
-                                                                                     sb.AppendLine("[Example(20,5,15)]");
-                                                                                     sb.AppendLine("public void Eating(int start, int eat, int left)");
-                                                                                     sb.AppendLine("{");
-                                                                                     sb.AppendLine("\"Given there are {0} cucumbers\".Given(() => {});");
-                                                                                     sb.AppendLine("\"When I eat {1} cucumbers\".When(() => {});");
-                                                                                     sb.AppendLine("\"Then I should have {2} cucumbers\".Then(() => {});");
-                                                                                     sb.AppendLine("}");
+                              sb.AppendLine("[Scenario]");
+                              sb.AppendLine("[Example(12,5,7)]");
+                              sb.AppendLine("[Example(20,5,15)]");
+                              sb.AppendLine("public void Eating(int start, int eat, int left)");
+                              sb.AppendLine("{");
+                              sb.AppendLine("\"Given there are {0} cucumbers\".Given(() => {});");
+                              sb.AppendLine("\"When I eat {1} cucumbers\".When(() => {});");
+                              sb.AppendLine("\"Then I should have {2} cucumbers\".Then(() => {});");
+                              sb.AppendLine("}");
 
-                                                                                     environment.TextWritten.Should()
-                                                                                                .Be(sb.ToString());
-                                                                                 });
+                              environment.TextWritten.Should()
+                                         .Be(sb.ToString());
+                          });
+
+            "Then the first line emitted is the Scenario attribute"
+                .Then(() => environment.LinesWritten[0].Should()
+                                                       .Be("[Scenario]"));
+
+            "Then the second and third lines emitted should be the Example attribute"
+                .Then(() =>
+                          {
+                              environment.LinesWritten[1].Should()
+                                                         .Be("[Example(12,5,7)]");
+                              environment.LinesWritten[2].Should()
+                                                         .Be("[Example(20,5,15)]");
+                          });
+
+            "Then the fourth line emitted is the method name with parameters"
+                .Then(() => environment.LinesWritten[3].Should()
+                                                       .Be("public void Eating(int start, int eat, int left)"));
+
+            "Then the instructions emitted have had their placeholders replaced"
+                .Then(() =>
+                          {
+                              environment.LinesWritten[5].Should()
+                                                         .Be("\"Given there are {0} cucumbers\".Given(() => {});");
+                              environment.LinesWritten[6].Should()
+                                                         .Be("\"When I eat {1} cucumbers\".When(() => {});");
+                              environment.LinesWritten[7].Should()
+                                                         .Be("\"Then I should have {2} cucumbers\".Then(() => {});");
+                          });
         }
     }
 }
